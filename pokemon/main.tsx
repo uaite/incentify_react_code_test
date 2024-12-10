189faecbfab2d7f4cf41dd9dd23ff306b7425e87
uaite <eduardotimmers@gmail.com>
2024-12-10 11:36:07 -0300

    chore: move App file

--- pokemon/main.tsx
+++ pokemon/main.tsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './src/components/App.tsx';
-// import App from './src/components/App/index';
+import App from './src/components/App/index';
 import './index.css';
 import PokemonProvider from './src/context/Pokemon';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
--- pokemon/src/components/App/index.tsx
+++ pokemon/src/components/App/index.tsx
@@ -1,63 +1,33 @@
-import { getImageConfigFromType } from '../../helpers';
-import { usePokemon } from '../../context/Pokemon';
 import List from '../List';
-// import AbilitySvg from '../../assets/information-icons/ability.svg';
-// import HeightSvg from '../../assets/information-icons/height.svg';
-// import WeightSvg from '../../assets/information-icons/weight.svg';
-// import SpeciesSvg from '../../assets/information-icons/species.svg';
+import DetailPage from '../DetailPage';
+import { usePokemon } from '../../context/Pokemon';
+import { useEffect } from 'react';
 
 function App() {
-  const { selected: selectedPkmn } = usePokemon();
-  const pkmnFirstType =
-    !!selectedPkmn && (selectedPkmn.types.length ?? 0) > 0
-      ? selectedPkmn.types[0]
-      : 'normal';
+  const { list, fetchNextPage, setSelectedName } = usePokemon();
 
-  const { color, vectorSrc } = getImageConfigFromType(pkmnFirstType);
+  useEffect(() => {
+    setTimeout(() => {
+      fetchNextPage?.();
+    }, 3000);
+    setTimeout(() => {
+      fetchNextPage?.();
+    }, 6000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      <div className="w-full">
-        <div className="w-1/3">
-          <List items={[]} />
-        </div>
-        <div className="w-2/3">
-          {selectedPkmn ? (
-            <div>
-              <div>
-                <div style={{ backgroundColor: color }}></div>
-                <img src={vectorSrc} />
-                <div className="w-[150px] h-[150px]">{selectedPkmn?.name}</div>
-              </div>
-              <div>
-                {selectedPkmn?.types.map((type) => {
-                  const typeImageConfig = getImageConfigFromType(type);
-
-                  return (
-                    <div key={type}>
-                      <img src={typeImageConfig.pngSrc} />
-                    </div>
-                  );
-                })}
-              </div>
-              <div>
-                <div>Weight</div>
-                <div>{selectedPkmn.weight} kg</div>
-              </div>
-              <div>
-                <div>Height</div>
-                <div>{selectedPkmn.height} m</div>
-              </div>
-              <div>
-                <div>Species</div>
-                <div>{selectedPkmn.species}</div>
-              </div>
-              <div>
-                <div>Ability</div>
-                <div>{selectedPkmn.ability}</div>
-              </div>
-            </div>
-          ) : null}
+      <div className="w-full h-full max-h-full flex gap-6">
+        <List
+          items={list}
+          onItemClick={(name) => {
+            setSelectedName(name);
+          }}
+          className="w-1/3 max-w-[220px] bg-white"
+        />
+        <div className="w-full bg-white">
+          <DetailPage />
         </div>
       </div>
     </>
